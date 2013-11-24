fe0eee3b2c574f210eec024e80f60a61b6f9af67
maxikg <maxmax58@googlemail.com>
2013-11-24 13:42:42 +0100

    List-Command hinzugefügt

--- src/main/java/de/mccityville/bungeeportals/commands/BpCommand.java
+++ src/main/java/de/mccityville/bungeeportals/commands/BpCommand.java
@@ -4,9 +4,11 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 import de.mccityville.bungeeportals.BungeePortalsPlugin;
 import de.mccityville.bungeeportals.Constants;
+import de.mccityville.bungeeportals.portals.Portal;
 import de.mccityville.bungeeportals.portals.PortalCreationException;
 import de.mccityville.bungeeportals.portals.PortalFactory;
 
@@ -48,8 +50,47 @@ public class BpCommand implements CommandExecutor {
 			}
 			
 			return true;
+		} else if (args.length == 1 && "list".equalsIgnoreCase(args[0])) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage(Constants.ERROR_PREFIX + "You must execute this command as a player.");
+				return true;
+			}
+			
+			if (!sender.hasPermission("bungeeportals.admin.list")) {
+				sender.sendMessage(Constants.ERROR_PREFIX + "No permission to execute this command.");
+				return true;
+			}
+			
+			sender.sendMessage(Constants.INFO_PREFIX + "Portals in your current world: ");
+			StringBuilder sb = new StringBuilder();
+			for (Portal portal : pf.getManager().getPortals(((Player) sender).getWorld())) {
+				Vector minPos = portal.getMinPos();
+				Vector maxPos = portal.getMaxPos();
+				
+				if (sb.length() > 0)
+					sb.append(", ");
+				
+				sb.append("{to:")
+					.append(portal.getTarget())
+					.append("; pos:")
+					.append(minPos.getX())
+					.append(",")
+					.append(minPos.getY())
+					.append(",")
+					.append(minPos.getZ())
+					.append("|")
+					.append(maxPos.getX())
+					.append(",")
+					.append(maxPos.getY())
+					.append(",")
+					.append(maxPos.getZ())
+					.append("}");
+			}
+			sb.insert(0, Constants.INFO_COLOR);
+			sender.sendMessage(sb.toString());
 		}
 		
+		sender.sendMessage(Constants.ERROR_PREFIX + "Invalid command usage.");
 		return false;
 	}
 }
